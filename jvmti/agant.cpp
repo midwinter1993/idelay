30161f1b7b80be41b9890f7dcb48d92d12385613
Dongjie Chen <midwinter1993@gmail.com>
2020-02-28 16:25:37 -0600

    Add options for jvmti agent

--- jvmti/agant.cpp
+++ jvmti/agant.cpp
@@ -1,12 +1,66 @@
 #include <jvmti.h>
 #include <stdio.h>
 #include <atomic>
+#include <string>
+
+#include <iostream>
 
 #include "utils.h"
 
+static bool g_opt_dump_thread = false;
+static int g_opt_num_spot_threads = 3;
+
+static bool is_number(const std::string& s) {
+    return !s.empty() && std::find_if(s.begin(),
+        s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+}
+
+static void parse_sub_option(const std::string &sub_opt) {
+    size_t pos = sub_opt.find(":");
+    if (pos == std::string::npos) {
+        return;
+    }
+    std::string key = sub_opt.substr(0, pos);
+    std::string value = sub_opt.substr(pos + 1);
+
+    if (key == "dump_thread" && value == "true") {
+        g_opt_dump_thread = true;
+        fprintf(stderr, "[ ! set dump thread ]\n");
+        return;
+    }
+    if (key == "num_spot_threads") {
+        if (!is_number(value)) {
+            return;
+        }
+        g_opt_num_spot_threads = std::stoi(value);
+        fprintf(stderr, "[ ! #Thread to start working %d ]\n", g_opt_num_spot_threads);
+    }
+}
+
+static void parse_options(const char *options) {
+    if (!options) {
+        return;
+    }
+    std::string delimiter = ",";
+
+    std::string buf{options};
+    if (buf.back() != ',') {
+        buf.push_back(','); // Append a delimiter for while loop
+    }
+
+    size_t pos = 0;
+    std::string sub_opt;
+    while ((pos = buf.find(delimiter)) != std::string::npos) {
+        sub_opt = buf.substr(0, pos);
+        // fprintf(stderr, "%s\n", sub_opt.c_str());
+        parse_sub_option(sub_opt);
+        buf.erase(0, pos + delimiter.length());
+    }
+}
+
 static std::atomic_int g_num_threads{0};
 
-void invodeStateMethod(JNIEnv *jni_env, const char *name) {
+static void invodeStateMethod(JNIEnv *jni_env, const char *name) {
     static const char *STATE_CLASS_NAME = "io/github/midwinter1993/State";
     jclass cls = jni_env->FindClass(STATE_CLASS_NAME);
     if (cls == nullptr) {
@@ -33,22 +87,24 @@ void JNICALL ThreadStart(jvmtiEnv* jvmti, JNIEnv* jni_env, jthread thread) {
     if (err_code == 0) {
         int num = g_num_threads.fetch_add(1, std::memory_order_seq_cst) + 1;
 
+        if (g_opt_dump_thread) {
+            fprintf(stderr, "Thread started: %s #%d\n", info.name, num);
+
+            jvmtiThreadGroupInfo group_info;
+            err_code = jvmti->GetThreadGroupInfo(info.thread_group, &group_info);
+            CHECK_ERROR("Get thread group failure");
+            if (!err_code) {
+                fprintf(stderr, "Thread group: %s\n", group_info.name);
+            }
+        }
+
         //
         // An approximated for multiple thread created by applications
         // Main thread, Signal Dispatcher, new thread
         //
-        if (num == 3) {
+        if (num == g_opt_num_spot_threads) {
             invodeStateMethod(jni_env, "startWork");
         }
-
-        // fprintf(stderr, "Thread started: %s %d\n", info.name, x+1);
-
-        // jvmtiThreadGroupInfo group_info;
-        // err_code = jvmti->GetThreadGroupInfo(info.thread_group, &group_info);
-        // CHECK_ERROR("Get thread group failure");
-        // if (!err_code) {
-            // fprintf(stderr, "Thread group: %s\n", group_info.name);
-        // }
     }
 }
 
@@ -67,6 +123,8 @@ void JNICALL ThreadEnd(jvmtiEnv* jvmti, JNIEnv* env, jthread thread) {
 jvmtiEnv* g_jvmti = NULL;
 
 JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM* vm, char* options, void* reserved) {
+
+    parse_options(options);
 
     vm->GetEnv((void**) &g_jvmti, JVMTI_VERSION_1_0);
 
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 AGENT="$PWD/target/idelay-1.0-SNAPSHOT.jar"
 JVMTI_AGENT="/$PWD/jvmti/x.so"
+# JVMTI_AGENT="/$PWD/jvmti/x.so=dump_thread:true,num_spot_threads:3"
 
 java -Dlog4j.configurationFile=log4j2.xml \ 
      -javaagent:$AGENT \
