035b15fd3d068e8ae5e2f7036c32a13461774ae6
Dongjie Chen <midwinter1993@gmail.com>
2020-04-09 17:02:58 -0500

    Change solver

--- log-analyze/constraint.py
+++ log-analyze/constraint.py
@@ -1,10 +1,57 @@
 from typing import List, Dict, Set, Tuple
 from litelog import LogEntry
-from pulp import LpVariable, lpSum, LpMinimize
-from pulp import LpProblem, LpConstraint, LpConstraintGE, LpStatus
-
+# from pulp import LpVariable, lpSum, LpMinimize
+# from pulp import LpProblem, LpConstraint, LpConstraintGE, LpStatus
+from flipy import LpVariable
+import flipy
 
 LocationId = int
+
+class LpBuilder:
+    cons_counter = 0
+
+    @classmethod
+    def var(cls, name: str, up_bound: int):
+        return flipy.LpVariable(name, var_type=flipy.VarType.Integer,
+                                low_bound=0, up_bound=up_bound)
+
+    @classmethod
+    def sum_expr(cls, lp_var_list: List[LpVariable]):
+        return flipy.LpExpression(expression={v: 1 for v in lp_var_list})
+
+    @classmethod
+    def const_expr(cls, value: int):
+        return flipy.LpExpression(constant=value)
+
+    @classmethod
+    def problem(cls, name: str):
+        return flipy.LpProblem(name)
+
+    @classmethod
+    def _cons_id(cls):
+        cls.cons_counter += 1
+        return cls.cons_counter
+
+    @classmethod
+    def constraint_sum_geq(cls, lp_var_list: List[LpVariable], value: int):
+        lhs = cls.sum_expr(lp_var_list)
+        rhs = cls.const_expr(value)
+
+        return flipy.LpConstraint(lhs, 'geq', rhs, name=f'_C{cls._cons_id()}')
+
+    @classmethod
+    def constraint_sum_leq(cls, lp_var_list: List[LpVariable], value: int):
+        lhs = cls.sum_expr(lp_var_list)
+        rhs = cls.const_expr(value)
+
+        return flipy.LpConstraint(lhs, 'leq', rhs, name=f'_C{cls._cons_id()}')
+
+    @classmethod
+    def constraint_sum_eq(cls, lp_var_list: List[LpVariable], value: int):
+        lhs = cls.sum_expr(lp_var_list)
+        rhs = cls.const_expr(value)
+
+        return flipy.LpConstraint(lhs, 'eq', rhs, name=f'_C{cls._cons_id()}')
 
 
 class Variable:
@@ -19,8 +66,8 @@ class Variable:
         #
         # We amplify the probability as integer values in [0, 100]
         #
-        self.pulp_rel_var_ = LpVariable(self.as_str_rel(), 0, 100)
-        self.pulp_acq_var_ = LpVariable(self.as_str_acq(), 0, 100)
+        self.lp_rel_var_ = LpBuilder.var(self.as_str_rel(), up_bound=100)
+        self.lp_acq_var_ = LpBuilder.var(self.as_str_acq(), up_bound=100)
 
     def __str__(self):
         return str(self.uid_)
@@ -49,11 +96,11 @@ class Variable:
     def as_str_acq(self) -> str:
         return f'A{self.uid_}'
 
-    def as_pulp_rel(self) -> LpVariable:
-        return self.pulp_rel_var_
+    def as_lp_rel(self) -> LpVariable:
+        return self.lp_rel_var_
 
-    def as_pulp_acq(self) -> LpVariable:
-        return self.pulp_acq_var_
+    def as_lp_acq(self) -> LpVariable:
+        return self.lp_acq_var_
 
     @classmethod
     def get_variable(cls, loc: str, description: str) -> 'Variable':
@@ -108,85 +155,92 @@ class ConstaintSystem:
 
     def print_system(self):
         print("Variable Definition")
-        for loc, loc_id in Variable.variable_pool.items() :
-            print(f'   {loc}  {loc_id}')
+        for loc, var in Variable.variable_pool.items() :
+            print(f'   {loc}  {var.uid_}, {var.description_}')
 
         print("\nConstrains : ")
 
         for constraint in self.rel_constraints_:
             var_list = [var.as_str_rel() for var in constraint]
-            s = f'1 <= {" + ".join(var_list)} <= 2'
+            s = f'1 <= {" + ".join(var_list)}'
             print (s)
 
         for constraint in self.acq_constraints_:
             var_list = [var.as_str_acq() for var in constraint]
-            s = f'1 <= {" + ".join(var_list)} <= 2'
+            s = f'1 <= {" + ".join(var_list)}'
             print (s)
 
-    def _pulp_encode_rel(self):
+    def _lp_encode_rel(self):
         for constraint in self.rel_constraints_:
-            lp_var_list = [var.as_pulp_rel() for var in constraint]
+            lp_var_list = [var.as_lp_rel() for var in constraint]
 
             #
             # There is only one release operation
             #
-            penalty = LpVariable(f'Penalty{len(self.penalty_vars_)}', 0, 50)
+            penalty = LpBuilder.var(f'Penalty{len(self.penalty_vars_)}', up_bound=50)
             self.penalty_vars_.append(penalty)
 
-            self.prob_ += lpSum(lp_var_list) + penalty >= 100
-            self.prob_ += lpSum(lp_var_list) <= 199
+            lp_var_list.append(penalty)
 
-    def _pulp_encode_acq(self):
+            self.prob_.add_constraint(LpBuilder.constraint_sum_geq(lp_var_list, 100))
+            # self.prob_ += lpSum(lp_var_list) <= 199
+
+    def _lp_encode_acq(self):
         for constraint in self.acq_constraints_:
-            lp_var_list = [var.as_pulp_acq() for var in constraint]
+            lp_var_list = [var.as_lp_acq() for var in constraint]
 
             #
             # There is only one acquire operation
             #
-            penalty = LpVariable(f'Penalty{len(self.penalty_vars_)}', 0, 50)
+            penalty = LpBuilder.var(f'Penalty{len(self.penalty_vars_)}', up_bound=50)
             self.penalty_vars_.append(penalty)
 
-            self.prob_ += lpSum(lp_var_list) + penalty >= 100
-            self.prob_ += lpSum(lp_var_list) <= 199
+            lp_var_list.append(penalty)
+            self.prob_.add_constraint(LpBuilder.constraint_sum_geq(lp_var_list, 100))
+            # self.prob_ += lpSum(lp_var_list) <= 199
 
-    def _pulp_encode_all_vars(self):
+    def _lp_encode_all_vars(self):
         #
         # For each variable/location, P_rel + P_acq < 100
         #
         for var in Variable.variable_pool.values():
-            self.prob_ += var.as_pulp_acq() + var.as_pulp_rel() <= 100
+            self.prob_.add_constraint(LpBuilder.constraint_sum_leq([var.as_lp_acq(), var.as_lp_rel()], 100))
 
-    def _pulp_encode_all_vars_heuristic(self):
+    def _lp_encode_all_vars_heuristic(self):
         for var in Variable.variable_pool.values():
             if 'Monitor.Enter' in var.description_:
-                self.prob_ += var.as_pulp_acq() == 100
+                self.prob_.add_constraint(LpBuilder.constraint_sum_eq([var.as_lp_acq()], 100))
             elif 'Monitor.Exit' in var.description_:
-                self.prob_ += var.as_pulp_rel() == 100
+                self.prob_.add_constraint(LpBuilder.constraint_sum_eq([var.as_lp_rel()], 100))
 
-    def _pulp_encode_object_func(self):
+    def _lp_encode_object_func(self):
         #
         # Object function: min(penalty + all variables)
         #
         obj_func_vars = self.penalty_vars_
 
         for var in Variable.variable_pool.values():
-            obj_func_vars.append(var.as_pulp_acq())
-            obj_func_vars.append(var.as_pulp_rel())
+            obj_func_vars.append(var.as_lp_acq())
+            obj_func_vars.append(var.as_lp_rel())
 
-        self.prob_ += lpSum(obj_func_vars)
+        obj = flipy.LpObjective(expression={v: 1 for v in obj_func_vars}, sense=flipy.Minimize)
 
-    def pulp_solve(self):
-        self.prob_ = LpProblem("HB_Infer", LpMinimize)
+        self.prob_.set_objective(obj)
+
+    def lp_solve(self):
+        self.prob_ = LpBuilder.problem("HB_Infer")
         self.penalty_vars_ = []
 
-        self._pulp_encode_rel()
-        self._pulp_encode_acq()
-        self._pulp_encode_all_vars()
-        self._pulp_encode_all_vars_heuristic()
-        self._pulp_encode_object_func()
+        self._lp_encode_rel()
+        self._lp_encode_acq()
+        self._lp_encode_all_vars()
+        self._lp_encode_all_vars_heuristic()
+        self._lp_encode_object_func()
 
-        status = self.prob_.solve()
-        print('Solving Status:', LpStatus[status])
+        solver = flipy.CBCSolver()
+        status = solver.solve(self.prob_)
+
+        print('Solving Status:', status)
 
         # for name, var in Variable.variable_pool.items():
         #     if var.as_pulp_acq().varValue >= 95 or var.as_pulp_rel().varValue >= 95:
@@ -201,11 +255,13 @@ class ConstaintSystem:
         print()
         print("Releasing sites :")
         for name, var in Variable.variable_pool.items():
-            if (var.as_pulp_rel().varValue >= 95):
+            if (var.as_lp_rel().evaluate() >= 95):
                 print(f'{var.description_} @ {name} => {var.as_str_rel()}')
 
         print("Acquiring sites :")
         for name, var in Variable.variable_pool.items():
-            if (var.as_pulp_acq().varValue >= 95):
+            if (var.as_lp_acq().evaluate() >= 95):
                 print(f'{var.description_} @ {name} => {var.as_str_acq()}')
+
+        self.prob_.write_lp(open('./problem.lp', 'w'))
 
